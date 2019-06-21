67f258306772872cb4f066cced4375e5122850e1
manish <manishkumar17963@gmail.com>
2019-06-21 10:20:49 +0530

    jwt_secret added

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -4,7 +4,7 @@ const getUserId=(request,requireAuth=true)=>{
 
    if(header){
     const token=header.replace('Bearer ','')
-    const decoded=jwt.verify(token,'ilovemyfamiliesandfriends')
+    const decoded=jwt.verify(token,JWT_SECRET)
     return decoded.userId
    }
    if(requireAuth){
