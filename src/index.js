f4600102dfbd7cf2379b71fb9228b5c66e08e88a
manish <manishkumar17963@gmail.com>
2019-06-25 13:57:49 +0530

    github

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 import '@babel/polyfill/noConflict'
-import { GraphQLServer, PubSub } from 'graphql-yoga'
-import db from './db'
-import {resolvers,fragmentReplacements} from './resolvers/index'
-import prisma from './prisma'
+import server from './server'
 
-
-
-const pubsub = new PubSub()
-
-const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers,
-    context(request){
-        return {
-            db,
-            pubsub,
-            prisma,
-            request
-        }
-    },
-    fragmentReplacements
-})
-
-server.start({port:process.env.PORT || 4000},() => {
+server.start({port:process.env.PORT || 5000},() => {
     console.log('The server is up!')
 })
